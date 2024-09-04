f8754b5b0539a6fb0bd5fd62e25d78ed8c0e9f1c
Frane <franezunic7@gmail.com>
2024-09-04 20:45:20 +0200

    Minor configuration files update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,37 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
+import pluginTs from '@typescript-eslint/eslint-plugin'
+import parserTs from '@typescript-eslint/parser'
 
 export default [
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  pluginJs.configs.recommended
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: parserTs,
+      parserOptions: {
+        ecmaVersion: 2016,
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': pluginTs
+    },
+    rules: {}
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs'
+    },
+    plugins: {
+      '@typescript-eslint': pluginTs
+    },
+    rules: {}
+  },
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node }
+    },
+    ...pluginJs.configs.recommended
+  }
 ]
